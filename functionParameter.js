c1c92d39aaf8a05bddd3fb0961a1afc2ec814790
MuhammadZikri <zikrim577@gmail.com>
2023-01-24 10:49:40 +0700

    Function Parameter - Javascript

---
+++ functionParameter.js
@@ -0,0 +1,9 @@
+// membuat function dengan parameter
+function perhitungan(angka1, angka2){
+    let jumlah = angka1 * angka2;
+    console.log(`Perhitungannya yaitu : ${jumlah}`);
+}
+
+perhitungan(3, 56);
+
+
